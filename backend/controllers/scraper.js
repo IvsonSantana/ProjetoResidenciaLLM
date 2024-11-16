72a5e8ce79420e324df9690072115e62e935538a
IvsonSantana <Ivson2323@gmail.com>
2024-11-15 21:07:50 -0300

    Funcionando sem JSON

--- backend/controllers/scraper.js
+++ backend/controllers/scraper.js
@@ -1,11 +1,28 @@
+const puppeteer = require('puppeteer');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function scrapePage(url) {
   try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto('https://www.linkedin.com/login');
+    await page.type('#username', 'ivson2323@gmail.com');
+    await page.type('#password', '83701939Iv*');
+    await page.click('[type="submit"]');
+    await page.waitForNavigation();
+  
+
+    const cookies = await page.cookies();
+    await browser.close();
+
     const { data } = await axios.get(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'Cookie': cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ')
       }
     });
     const $ = cheerio.load(data);
